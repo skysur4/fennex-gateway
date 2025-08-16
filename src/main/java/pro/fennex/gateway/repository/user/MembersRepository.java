9ad87cbde94346af91b5937857d452187aa36de6
Chris Kim <skysur4@hotmail.com>
2025-08-16 14:20:12 +0900

    멤버 목록에 여러줄 나오는 오류 수정

--- src/main/java/pro/fennex/gateway/repository/user/MembersRepository.java
+++ src/main/java/pro/fennex/gateway/repository/user/MembersRepository.java
@@ -10,52 +10,42 @@ import reactor.core.publisher.Mono;
 
 @Repository
 public interface MembersRepository extends R2dbcRepository<Members, String> {
-    static final String SELECT_QUERY = "select" +
-            "    m.nickname" +
-            "    , m.union_name" +
-            "    , m.team_used1" +
-            "    , m.team_used2" +
-            "    , m.team_used3" +
-            "    , m.updated_at" +
-            "    , case" +
-            "        when s.level = '1'" +
-            "        then s.cnt > 0" +
-            "        else false" +
-            "      end as simulated_level1" +
-            "    , case" +
-            "        when s.level = '2'" +
-            "        then s.cnt > 0" +
-            "        else false" +
-            "      end as simulated_level2" +
-            "    , case" +
-            "        when s.level = '3'" +
-            "        then s.cnt > 0" +
-            "        else false" +
-            "      end as simulated_level3" +
-            "  from members m" +
-            "  left" +
-            "  join (select" +
-            "            nickname" +
-            "            , level" +
-            "            , nullif(count(*), 0) as cnt" +
-            "          from score s" +
-            "         group by nickname, level" +
-            "        ) s" +
-            "     on s.nickname = m.nickname";
+    static final String SELECT_QUERY = "select " +
+            "    m.nickname " +
+            "    , m.union_name " +
+            "    , m.team_used1 " +
+            "    , m.team_used2 " +
+            "    , m.team_used3 " +
+            "    , m.updated_at " +
+            "    , coalesce(max(s.simulated_level1) > 0, false) as simulated_level1 " +
+            "    , coalesce(max(s.simulated_level2) > 0, false) as simulated_level2 " +
+            "    , coalesce(max(s.simulated_level3) > 0, false) as simulated_level3 " +
+            "  from members m " +
+            "  left " +
+            "  join (select " +
+            "            nickname " +
+            "           , case when s.level = '1' then count(s.level) else 0 end as simulated_level1 " +
+            "           , case when s.level = '2' then count(s.level) else 0 end as simulated_level2 " +
+            "           , case when s.level = '3' then count(s.level) else 0 end as simulated_level3 " +
+            "           from score s " +
+            "           group by nickname, level " +
+            "        ) s " +
+            "     on s.nickname = m.nickname" +
+            "  group by m.nickname ";
 
     @Query(value = SELECT_QUERY +
             " order by m.nickname")
     Flux<Members> findAllMembers();
 
     @Query(value =
-        "  insert into members" +
-        "  select distinct false" +
-        "         , false" +
-        "         , false" +
-        "         , current_timestamp" +
-        "         , s.nickname" +
-        "         , s.union_name" +
-        "    from score s" +
+        "  insert into members " +
+        "  select distinct false " +
+        "         , false " +
+        "         , false " +
+        "         , current_timestamp " +
+        "         , s.nickname " +
+        "         , s.union_name " +
+        "    from score s " +
         "   where s.nickname not in (select distinct nickname from members)")
     Mono<Void> saveAuto();
 
